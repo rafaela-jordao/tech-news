069c0a7f58f72e20ffa1e61b971a1267244c8aef
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-11-09 15:47:22 -0300

    feat: cria a função scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -34,7 +34,28 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    comments_count = selector.css("div.comment-respond").getall()
+    summary = selector.css(
+        "div.entry-content > p:nth-of-type(1) *::text"
+    ).getall()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": len(comments_count),
+        "summary": "".join(summary).strip(),
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5
