964c4625268d8a3f989a6c651b97513a57c05903
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-11-16 10:01:36 -0300

    feat: cria a função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,11 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_news = get_collection().find({})
+    news = top_news.sort([("comments_count", -1), ("title", 1)])
+    return [(new["title"], new["url"]) for new in news[:5]]
 
 
 # Requisito 11
